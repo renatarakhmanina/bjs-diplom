5d2f488ce79f0de1267fad63469fe79e93696caa
Renata Rakhmanina <renatarahmanina@gmail.com>
2023-10-31 23:14:40 +0300

    Initial commit

---
+++ public/loginPage.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const { response } = require("express");
+
+const userForm = new UserForm();
+userForm.loginFormCallback = data => {
+  ApiConnector.login(data, response => {
+    if (response.success) {
+      location.reload();
+    } else {
+      setLoginErrorMessage('Ошибка. Пользователь не наден.');
+    }
+  });
+}
+
+userForm.registerFormCallback = data => {
+  ApiConnector.register(data, response => {
+    if (response.success) {
+      location.reload();
+    } else {
+      setRegisterErrorMessage('Ошибка. Введите другие данные для регистрации.');
+    }
+  });
+}
